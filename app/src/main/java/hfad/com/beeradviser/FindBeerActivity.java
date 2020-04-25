b985349b9b7e7266015ce75706b8eb78c8238cf9
toyan <toyan.harvey@gmail.com>
2020-04-25 05:49:54 -0700

    test commit

---
+++ app/src/main/java/hfad/com/beeradviser/FindBeerActivity.java
@@ -0,0 +1,25 @@
+package hfad.com.beeradviser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class FindBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer) ;
+    }
+
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+    }
+
+
+}
