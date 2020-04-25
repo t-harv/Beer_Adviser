685de455a7b4e46bc0720406b821d907f316c72b
toyan <toyan.harvey@gmail.com>
2020-04-25 13:25:25 -0700

    adding logic for the app

--- app/src/main/java/hfad/com/beeradviser/FindBeerActivity.java
+++ app/src/main/java/hfad/com/beeradviser/FindBeerActivity.java
@@ -6,7 +6,11 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class FindBeerActivity extends Activity {
+
+    BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,19 @@ public class FindBeerActivity extends Activity {
     }
 
     public void onClickFindBeer(View view){
-        TextView brands = (TextView) findViewById(R.id.brands);
-        Spinner color = (Spinner) findViewById(R.id.color);
+        TextView brands = findViewById(R.id.brands);
+        Spinner color = findViewById(R.id.color);
         String beerType = String.valueOf(color.getSelectedItem());
-        brands.setText(beerType);
+
+//        List<String> results = expert.getBrands(beerType);
+//        brands.setText(results.toString());
+
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for(String brand: brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted.toString());
     }
 
 
